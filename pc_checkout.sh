eadd5f79be42043177a4fab2ad866ddd7c1b0afb
AokiMinoruS63 <personal-git@aokiminoru.work>
2023-11-08 20:00:16 +0900

    プラットフォーム切り替えファイル内の処理を修正

--- pc_checkout.sh
+++ pc_checkout.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-# `Cargo.toml` をPC向けに書き換える
-cat > Cargo.toml <<EOL
-[package]
-name = "bevy_pc_example"
-version = "0.1.0"
-edition = "2021"
-description = "Example for building a PC app with Bevy"
-publish = false
-license = "MIT OR Apache-2.0"
-
-# PC向けの依存関係を追加
-[dependencies]
-bevy = "0.12.0"
-# wasm向けの依存関係を追加
-[target.wasm32-unknown-unknown]
-runner = "wasm-server-runner"
-EOL
-
-# `main.rs`をコピーし、`lib.rs`を削除
-if [ ! -f src/main.rs ]; then
-    cp template/main.rs src/main.rs
-    rm src/lib.rs
-fi
+# `Cargo.toml` をPC向けに置き換える
+cp platform_setting/PcCargo.toml Cargo.toml
+# `lib.rs`を`main.rs`にリネーム
+mv src/lib.rs src/main.rs
 
 echo "PC environment is set up."
